0ea0e787bcdde92e1de319efac77381ad6f64c13
adoubleb <abdulhakamnr@gmail.com>
2022-01-07 19:46:25 +0800

    Homepage

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def index(request):
-    return HttpResponse('hello world')
+    return render(request, 'pages/index.html')
 # Create your views here.
+
+
+def about(request):
+    return render(request, 'pages/about.html')
