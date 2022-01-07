ab844f3d976fbd860ccf36ca2a6e3bbe62225a14
adoubleb <abdulhakamnr@gmail.com>
2022-01-08 00:34:09 +0800

    added tutors and assignments models

--- tutors/models.py
+++ tutors/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Assignments(models.Model):
+    subject = models.CharField(max_length=30)
+    level = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
+    rateperhour = models.IntegerField()
+    gender = models.CharField(max_length=30)
+    subject = models.CharField(max_length=30)
+    desc = models.TextField()
+    date_posted = models.DateTimeField(default=datetime.now)
