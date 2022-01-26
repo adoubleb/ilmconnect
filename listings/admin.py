8fd560a76465a76942d5b71e14bcb31ee9b87ced
adoubleb <abdulhakamnr@gmail.com>
2022-01-26 21:22:54 +0800

    cleanup site

--- listings/admin.py
+++ listings/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Tutors
+from .models import Tutors, Endorsements
 
 class TutorAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'subjects', 'locations')
     list_display_links = ('id','name')
+class EndorsementsAdmin(admin.ModelAdmin):
+    list_display = ('target', 'endorsers')
 admin.site.register(Tutors, TutorAdmin)
+admin.site.register(Endorsements, EndorsementsAdmin)
+
